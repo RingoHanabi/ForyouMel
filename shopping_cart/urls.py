6fdb9fc41b1dd88241bb8d287079edfcb8b102a7
Nelly YuanHong Li <ylii0204@student.monash.edu>
2021-12-13 17:08:17 +1100

    no message

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -6,6 +6,4 @@ from shopping_cart import views
 urlpatterns = [
     path('en/', views.shopping_cart_en, name = "shopping cart en"),
     path('cn/', views.shopping_cart_cn, name = "shopping cart cn"),
-    path('checkout/', views.checkout_en, name = "checkout en"),
-    path('checkout/cn/', views.checkout_cn, name = "checkout cn"),
 ]
