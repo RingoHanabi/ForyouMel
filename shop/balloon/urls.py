30caeef2ac15db3a89bce1c029aa71a7bf7ccb25
Nelly YuanHong Li <ylii0204@student.monash.edu>
2022-02-09 16:12:10 +1100

    no message

--- shop/balloon/urls.py
+++ shop/balloon/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.balloon_en, name = "balloon"),
     path('cn/', views.balloon_cn, name = "balloon cn"),
+    path('type/<type_id>', views.balloon_type_en, name="balloon type"),
+    path('type/cn/<type_id>', views.balloon_type_cn, name="balloon type cn"),
     path('details/<id>/', views.balloon_details_en, name="balloon details"),
     path('details/cn/<id>/', views.balloon_details_cn, name="balloon details cn"),
     path('post_main_type/', views.post_balloon_maintypes, name="balloon main type post"),
--- shop/balloon/views.py
+++ shop/balloon/views.py
@@ -44,24 +44,54 @@ def balloon_details_cn(request,id):
 
     return render(request, 'balloon_details_cn.html',{'page_title':"气球装饰: "+balloon_detail.name_cn,"balloon":balloon_detail})
 
+def balloon_type_en(request,type_id):
+    if request.method == 'POST' and request.POST["language"] == "English":
+        return redirect("balloon type", type_id)
+    if request.method == 'POST' and request.POST["language"] == "Chinese":
+        return redirect("balloon type cn", type_id)
+
+    all_balloons = balloon.objects.all()
+    all_balloon_types = balloon_type.objects.all()
+    all_balloon_sub_types = balloon_sub_type.objects.all()
+    this_type = all_balloon_sub_types.filter(id=type_id).first()
+
+    return render(request, 'balloons_type.html',
+                  {'page_title': "Our balloons", "types": all_balloon_types, "sub_types": all_balloon_sub_types,
+                   "balloons": all_balloons, "this_type": this_type})
+
+
+def balloon_type_cn(request, type_id):
+    if request.method == 'POST' and request.POST["language"] == "English":
+        return redirect("balloon type", type_id)
+    if request.method == 'POST' and request.POST["language"] == "Chinese":
+        return redirect("balloon type cn", type_id)
+
+    all_balloons = balloon.objects.all()
+    all_balloon_types = balloon_type.objects.all()
+    all_balloon_sub_types = balloon_sub_type.objects.all()
+    this_type = all_balloon_sub_types.filter(id=type_id).first()
+
+    return render(request, 'balloons_type_cn.html',
+                  {'page_title': "Our balloons", "types": all_balloon_types, "sub_types": all_balloon_sub_types,
+                   "balloons": all_balloons, "this_type": this_type})
+
 def post_balloon_maintypes(request):
     if request.is_ajax and request.method == "POST":
         main_types = balloon_type.objects.all()
         return_data = []
         for each in main_types:
-            return_data.append([each.id,each.name_cn])
+            return_data.append([each.id, each.name_cn])
         return_data = json.dumps(return_data)
-        return JsonResponse(return_data, status=200,safe=False)
+        return JsonResponse(return_data, status=200, safe=False)
 
 
 def post_balloon_subtypes(request):
     if request.is_ajax and request.method == "POST":
-        main_type_id = request.POST.get("main_type_id",None)
-        sub_types = balloon_sub_type.objects.all().filter(main_type = main_type_id)
+        main_type_id = request.POST.get("main_type_id", None)
+        sub_types = balloon_sub_type.objects.all().filter(main_type=main_type_id)
         return_data = []
         for each in sub_types:
             return_data.append([each.id, each.__str__()])
 
         return_data = json.dumps(return_data)
-        return JsonResponse(return_data, status=200,safe=False)
-
+        return JsonResponse(return_data, status=200, safe=False)
